af0dc61bee6cd4d63233410ea500799c1ccbcd7d
HernandezJC <jchernandez1021@gmail.com>
2023-08-04 12:52:11 -0700

    made snapshot for app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+          {/* Example to make test fail: */}
+        {/* <h2>hello</h2> */}
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/GitHubCard/GitHubCard.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Card from "react-bootstrap/Card"
+'
+
+function GitHubCard() {
+  return (
+      <div className="card">
+        <Card style={{ width: "18rem"}}>
+        
+        
+        <Card.Img variant="top" src={} />
+        
+        <Card.Body>
+            <Card.Title>Mickey Mouse</Card.Title>
+            <Card.Text>
+
+            </Card.Text>
+        </Card.Body>
+        </Card>
+      </div>
+  );
+}
+
+export default GitHubCard
